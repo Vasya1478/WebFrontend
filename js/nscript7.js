a6a4c1a49987d311fe0d97df2a887663b1994d77
Vasya1478 <mdm.59@mail.ru>
2019-12-02 21:54:29 +0300

    Релиз 4.8

--- js/nscript7.js
+++ js/nscript7.js
@@ -1,11 +1,11 @@
-const arr = [10, 12, 15, 21];
-for (var i = 0; i < arr.length; i++) {
-(function(i) {
-    setTimeout(function() {
-        console.log('Index: ' + i + ', element: ' + arr[i]);
-        }, 3000);
-}) (i);
-}
+// const arr = [10, 12, 15, 21];
+// for (var i = 0; i < arr.length; i++) {
+// (function(i) {
+//     setTimeout(function() {
+//         console.log('Index: ' + i + ', element: ' + arr[i]);
+//         }, 3000);
+// }) (i);
+// }
 
 
 const arr_2 = [10, 12, 15, 21];
